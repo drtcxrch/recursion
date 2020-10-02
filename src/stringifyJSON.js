1d695ec0eb71b61a15a3f8e4ecf544a5e6cc0c9e
Grant Taggart <granttaggartsf@gmail.com>
2020-10-02 09:59:01 -0700

    Complete stringifyJSON
    Loop through objects and recursively stringify each value within the object and return the object at the end. Objects that contain unstringifiable values(functions and undefined) should just return an empty strung object.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,19 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if (typeof obj === 'number') {
-    return '' + obj;
-  }
-
-  if (obj === null) {
-    return '' + obj;
-  }
-
-  if (typeof obj === 'boolean') {
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
     return '' + obj;
   }
 
   if (typeof obj === 'string') {
     return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var strungArr = '[';
+
+    for (var i = 0; i < obj.length; i++) {
+      if (i !== obj.length - 1) {
+        strungArr += stringifyJSON(obj[i]) + ',';
+      } else {
+        strungArr += stringifyJSON(obj[i]);
+      }
+    }
+
+    return strungArr + ']';
+
+  } else {
+    var strungObj = '{';
+
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        strungObj += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+
+    if (strungObj.length > 2) {
+      return strungObj.slice(0, strungObj.length - 1) + '}';
+    } else {
+      return strungObj + '}';
+    }
   }
 };
